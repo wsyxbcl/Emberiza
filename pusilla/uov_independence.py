8695b238bf8fa793f15bcf0201eb6a7049d1a296
wsyxbcl <wsyxbcl@gmail.com>
2024-02-26 23:26:47 +0800

    fix dataframe datetime format and add dummy path for serval compatibility

--- pusilla/uov_independence.py
+++ pusilla/uov_independence.py
@@ -21,10 +21,10 @@ if __name__ == '__main__':
     csv_dir = Path(sys.argv[1])
     uov_dfs = [pl.read_csv(f, encoding='GBK') for f in csv_dir.glob("*.csv")]
     uov_filenames = [Path(f).stem for f in csv_dir.glob("*.csv")]
-    print(uov_filenames)
+
     # Concatinate
     for i, df in enumerate(uov_dfs):
-        df = df.with_columns(pl.lit(uov_filenames[i]).alias("path"))
+        df = df.with_columns(pl.lit(os.sep+uov_filenames[i]+os.sep+"dummy").alias("path"))
         if i == 0:
             uov_data = df
         else:
@@ -34,12 +34,12 @@ if __name__ == '__main__':
         pl.col("path"),
         pl.col("照片名").alias("filename"),
         pl.col("物种名称").alias("species"),
-        pl.col("时间").alias("datetime_original")])
+        pl.col("时间").str.strptime(pl.Datetime).alias("datetime_original")])
     # Create output directory and write to csv
     output_dir = csv_dir.joinpath("result")
     output_dir.mkdir(parents=True, exist_ok=True)
     output_csv_path = output_dir.joinpath("tags.csv")
-    uov_data.write_csv(output_csv_path)
+    uov_data.write_csv(output_csv_path, datetime_format=r"%Y-%m-%d %H:%M:%S")
     # Encode to GBK
     with open(output_csv_path, 'r', encoding='utf-8') as f:
         with open(output_dir.joinpath("tags_gbk.csv"), 'w', encoding='GBK') as f_out:
@@ -48,10 +48,9 @@ if __name__ == '__main__':
     # Temporal independence analysis
     df_cleaned = uov_data.lazy().select([
         pl.col("path").alias("deployment"),
-        pl.col("datetime_original").str.strptime(pl.Datetime).alias("time"),
+        pl.col("datetime_original").alias("time"),
         pl.col("filename"),
         pl.col("species")]).drop_nulls().unique(subset=["deployment", "time", "species"], maintain_order=True).collect()
-        # pl.col("species")]).collect()
     df_cleaned.write_csv(output_dir.joinpath("cleaned.csv"))
 
     df_sorted = df_cleaned.lazy().sort("time").sort("species").sort("deployment").collect()
