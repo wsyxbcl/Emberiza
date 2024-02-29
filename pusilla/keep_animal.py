9fff55ad1e2bac96ef3ac27dd8412d39a0eb2aea
wsyxbcl <wsyxbcl@gmail.com>
2024-02-29 14:02:01 +0800

    helper function for dispatch

---
+++ pusilla/keep_animal.py
@@ -0,0 +1,13 @@
+import argparse
+import os
+from shutil import copytree, ignore_patterns
+            
+if __name__ == "__main__":
+    dir_name_exclude = ["Blank", "Others", "Person", "Vehicle"]
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('src_dir')
+    parser.add_argument('dst_dir')
+    args = parser.parse_args()
+    
+    copytree(args.src_dir, args.dst_dir, ignore=ignore_patterns(*dir_name_exclude), dirs_exist_ok=True)
