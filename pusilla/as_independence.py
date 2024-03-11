45faa291c7f4b89e1d8e3bf91ed3ace818649688
wsyxbcl <wsyxbcl@gmail.com>
2024-03-11 15:44:32 +0800

    Specifying dtypes

--- pusilla/as_independence.py
+++ pusilla/as_independence.py
@@ -1,10 +1,10 @@
 # Workaround for serval capture on unaligned AS data
 import pandas as pd
 
-tags_csv_path = "~/Downloads/tags.csv"
-output_csv_path = "~/Downloads/tags_path_split.csv"
+tags_csv_path = "~/Downloads/tags_images.csv"
+output_csv_path = "~/Downloads/tags_images_path_split.csv"
 
-df = pd.read_csv(tags_csv_path)
+df = pd.read_csv(tags_csv_path, dtype={"species": "string", "individuals": "string"})
 
 # Path splitting
 split_values = df["path"].str.split("\\", expand=True)
