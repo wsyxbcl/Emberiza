311f6fa4fa218f107a3cc38d4910559272fb4d58
wsyxbcl <wsyxbcl@gmail.com>
2024-03-11 15:30:23 +0800

    temporal patch for Angsai data

---
+++ pusilla/as_independence.py
@@ -0,0 +1,14 @@
+# Workaround for serval capture on unaligned AS data
+import pandas as pd
+
+tags_csv_path = "~/Downloads/tags.csv"
+output_csv_path = "~/Downloads/tags_path_split.csv"
+
+df = pd.read_csv(tags_csv_path)
+
+# Path splitting
+split_values = df["path"].str.split("\\", expand=True)
+df["path"] = '/' + split_values[5] + '/' + split_values[6] + '/dummy.jpg'
+
+# Save
+df.to_csv(tags_csv_path)
