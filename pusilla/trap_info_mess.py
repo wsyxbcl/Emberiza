ddac995e90ef89f021e6f05299d068fb48545580
wsyxbcl <wsyxbcl@gmail.com>
2024-04-11 19:43:33 +0800

    accommodate data preprocessing

--- pusilla/trap_info_mess.py
+++ pusilla/trap_info_mess.py
@@ -6,21 +6,21 @@ if __name__ == '__main__':
     LATITUDE_PLACEHOLDER = 0
     LONGITUDE_PLACEHOLDER = 0
     COLLECTION_NAME = "AS202201-202203"
-    BATCH = 0
+    BATCH = 1
 
     filename = Path("trap_info.csv")
-    trap_info_path = Path("/mnt/data/AngSai/AS202201-202203")
+    trap_info_path = Path("/mnt/data/AngSai/batch3")
+
     if BATCH:
-        trap_infos = trap_info_path.glob('*/YunTa*.csv')
+        
+        trap_infos = trap_info_path.glob('*/trap_info.xlsx')
+        dtypes={"latitude": pl.Float32, "longitude": pl.Float32,}
         trap_info_raw = pl.concat(
             [
-                pl.read_csv(f, 
-                    encoding='GBK', 
-                    skip_rows=1, 
-                    dtypes={"deploymentName": pl.Utf8,
-                        "timestampProblem": pl.Boolean,
-                        "coordinateUncertainty": pl.Utf8,})
-                        .with_columns(pl.lit(Path(f).parent.name).alias("collectionName"))
+                pl.read_excel(f,
+                    engine_options={"dateformat": "%m/%d/%Y"},
+                    read_options={"skip_rows": 1, "dtypes": dtypes, "infer_schema_length": 0}
+                ).with_columns(pl.lit(Path(f).parent.name).alias("collectionName"))
                 for f in trap_infos
             ],
             how="diagonal"
@@ -29,6 +29,10 @@ if __name__ == '__main__':
         trap_info_raw = pl.read_csv(trap_info_path.joinpath(filename),
             # encoding='GBK',
             skip_rows=1).with_columns(pl.lit(COLLECTION_NAME).alias("collectionName"))
+
+    # # Check the datetime format
+    # with pl.Config(tbl_rows=-1):
+    #     print(trap_info_raw.select(["deploymentName", "collectionName", "deploymentStart", "deploymentEnd"]))
 
     # Drop nulls (for some reason there're empty rows in csv)
     trap_info_cleaned = trap_info_raw.filter(~pl.all_horizontal(pl.col("deploymentName").is_null())) 
@@ -48,8 +52,12 @@ if __name__ == '__main__':
         # ISO 8601 / RFC 3339 date & time format
         pl.col("deploymentStart", "deploymentEnd")
             .str.to_datetime(format="%m/%d/%Y")
-            .dt.replace_time_zone(time_zone="Asia/Shanghai")
-            .dt.to_string("%Y-%m-%dT%H:%M:%S%:z"))
+            .dt.replace_time_zone(time_zone="Asia/Shanghai"),
+        pl.concat_str([
+            pl.col("deploymentName"),
+            pl.col("collectionName")
+            ], separator='_').alias("deploymentID"), 
+        )
 
     trap_info_tags = trap_info_cleaned.with_columns( 
         pl.when(pl.col("locationSource").eq("精确GPS"))
@@ -83,25 +91,21 @@ if __name__ == '__main__':
     )
     # print(trap_info_tags)
 
-    # Patch location
+    # Patch location and deploymentEnd
     trap_info_patch = trap_info_tags.with_columns(
         pl.col("latitude").fill_null(pl.lit(LATITUDE_PLACEHOLDER)), 
         pl.col("longitude").fill_null(pl.lit(LONGITUDE_PLACEHOLDER)),
+        (pl.col("deploymentEnd") + pl.duration(days=1)).alias("deploymentEnd")
     )
     # print(trap_info_patch)
 
     deployments = trap_info_patch.select(
-        pl.concat_str([
-            pl.col("deploymentName"),
-            pl.col("collectionName")
-        ], separator='_').alias("deploymentID"), 
-
-        pl.col("latitude",
+        pl.col("deploymentID",
+            "latitude",
             "longitude",
-            "deploymentStart",
-            "deploymentEnd",
             "setupBy", 
             "timestampIssues"),
+        pl.col("deploymentStart", "deploymentEnd").dt.to_string("%Y-%m-%dT%H:%M:%S%:z"), 
         pl.concat_str([
             pl.lit("有效工作"),
             pl.col("cameraWorkingDays"),
@@ -116,11 +120,7 @@ if __name__ == '__main__':
             pl.col("otherIssues")
         ], separator=" | ")
             .alias("deploymentTags"),
-        pl.concat_str([
-            pl.col("latitude"),
-            pl.col("longitude")
-        ], separator="/")
-            .alias("locationID"),
+        pl.col("deploymentID").alias("locationID"),
     )
     print(deployments)
     if BATCH:
