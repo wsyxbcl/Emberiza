7d21603a494232001f49e7d2906674cad3631b28
wsyxbcl <wsyxbcl@gmail.com>
2024-04-17 19:17:23 +0800

    fix: wrong column number, synonym treatment, add argparser

--- maze-taxonomy/patch_tags.py
+++ maze-taxonomy/patch_tags.py
@@ -3,51 +3,57 @@
 
 import argparse
 import polars as pl
+from pathlib import Path
 
-# exclude_tags = []
+# exclude_tags = [""]
+TAGLIST_PATH = Path(__file__).parent / "data/maze_taglist.csv"
+TAGDICT_PATH = Path(__file__).parent / "data/maze_tagdict.csv"
 
-df_maze_taglist = pl.read_csv("./data/maze_taglist.csv")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Patch tags.csv with tagdict and taglist')
+    parser.add_argument('tags', type=str, help='Input tags csv file')
+    parser.add_argument('-o', '--output', type=str, help='Output (patched) tags csv file', required=True)
 
-df_maze_tagdict = pl.read_csv("./data/maze_tagdict.csv", has_header=False)
-df_maze_tagdict = df_maze_tagdict.select([
-    pl.col("column_1").alias("tag"),
-    pl.concat_list([f"column_{i}" for i in range(2, 10)]).alias("synonym").list.drop_nulls()
-])
-maze_tagdict = dict(df_maze_tagdict.iter_rows())
-maze_synonyms_dict = {}
-for k in maze_tagdict.keys():
-    for v in maze_tagdict[k]:
-        maze_synonyms_dict[v] = k
+    args = parser.parse_args()
 
-df_tags_csv = pl.read_csv("/home/wsyxbcl/AngSai/batch2-align/tags.csv").drop_nulls()
+    df_maze_taglist = pl.read_csv(TAGLIST_PATH)
+    df_maze_tagdict = pl.read_csv(TAGDICT_PATH, has_header=False)
+    df_maze_tagdict_concat = df_maze_tagdict.select([
+        pl.col("column_1").alias("tag"),
+        pl.concat_list(df_maze_tagdict.columns[1:]).alias("synonym").list.drop_nulls()
+    ])
+    maze_tagdict = dict(df_maze_tagdict_concat.iter_rows())
+    maze_synonym_dict = {}
+    for k in maze_tagdict.keys():
+        for v in maze_tagdict[k]:
+            maze_synonym_dict[v] = k
 
+    df_tags_csv = pl.read_csv(args.tags).drop_nulls()
+    # Analyze tags and add case sensitivity to tagdict
+    tags_taglist = df_maze_taglist.select("tag").to_series().to_list()
+    tags_tagdict = df_maze_tagdict_concat.select("tag").to_series().to_list()
+    tags_csv = df_tags_csv.select("species").unique().to_series().to_list()
+    for tag in tags_csv:
+        if tag in tags_taglist:
+            continue
+        # elif tag in exclude_tags:
+        #     print(f"EXCLUDE: {tag}")
+        elif tag.lower() in (tags_taglist_lower := [t.lower() for t in tags_taglist]):
+            tag_corrected = tags_taglist[tags_taglist_lower.index(tag.lower())]
+            print(f"CASE INSENSITIVE: {tag} -> {tag_corrected}")
+            maze_synonym_dict[tag] = tag_corrected
+        elif tag.lower() in (maze_synonym_dict_lower := {k.lower(): v for k, v in maze_synonym_dict.items()}):
+            tag_corrected = maze_synonym_dict_lower[tag.lower()]
+            maze_synonym_dict[tag] = tag_corrected
+            print(f"SYNONYM CASE INSENSITIVE: {tag} -> {tag_corrected}")
+        elif tag in maze_synonym_dict:
+            tag_corrected = maze_synonym_dict[tag]
+            print(f"SYNONYM: {tag} -> {tag_corrected}")
+        else:
+            print(f"UNKNOWN: {tag}")
 
-# Analyze tags and add case sensitivity to tagdict
-tags_taglist = df_maze_taglist.select("tag").to_series().to_list()
-tags_tagdict = df_maze_tagdict.select("tag").to_series().to_list()
-tags_csv = df_tags_csv.select("species").unique().to_series().to_list()
-for tag in tags_csv:
-    if tag in tags_taglist:
-        continue
-    # elif tag in exclude_tags:
-    #     print(f"EXCLUDE: {tag}")
-        maze_synonyms_dict.setdefault(tag, []).append("")
-    elif tag.lower() in (tags_taglist_lower := [t.lower() for t in tags_taglist]):
-        tag_corrected = tags_taglist[tags_taglist_lower.index(tag.lower())]
-        print(f"CASE INSENSITIVE: {tag} -> {tag_corrected}")
-        maze_synonyms_dict.setdefault(tag, []).append(tag_corrected)
-    elif tag in maze_synonyms_dict:
-        tag_corrected = maze_synonyms_dict[tag]
-        print(f"SYNONYM: {tag} -> {tag_corrected}")
-    elif tag.lower() in (maze_synonyms_dict_lower := {k.lower(): v for k, v in maze_synonyms_dict.items()}):
-        tag_corrected = maze_synonyms_dict_lower[tag.lower()]
-        maze_synonyms_dict[tag] = tag_corrected
-        print(f"SYNONYM CASE INSENSITIVE: {tag} -> {tag_corrected}")
-    else:
-        print(f"NEW TAG: {tag}")
-
-# Patch tags
-df_tags_csv = df_tags_csv.with_columns(
-    pl.col("species").replace(maze_synonyms_dict).alias("tags")
-)
-df_tags_csv.write_csv("../test/tags_patched.csv")
+    # Patch tags
+    df_tags_csv = df_tags_csv.with_columns(
+        pl.col("species").replace(maze_synonym_dict).alias("tags")
+    )
+    df_tags_csv.write_csv(args.output)
