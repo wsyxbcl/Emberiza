b6e4bcbd1663aad14caa2c6ec6e8fcaca5a54e54
wsyxbcl <wsyxbcl@gmail.com>
2024-05-10 18:17:41 +0800

    handling sampling overflow

--- pusilla/resource_sample.py
+++ pusilla/resource_sample.py
@@ -5,23 +5,23 @@ import polars as pl
 
 ### Configuration
 RAMDOM_SEED = 42
-NUM_SAMPLES = 10000000
+NUM_SAMPLES = 100
 SPECIES_RATIO = {
     "Snow leopard": 0.2,
     "Blank": 0.5,
-    "Human": 0.3
+    "Human": 0.3,
 }
 TAGS_CSV_PATH = "/home/wsyxbcl/AngSai/batch_202304/tags.csv"
 
 
 output_csv_path = TAGS_CSV_PATH.replace(".csv", "_sampled.csv")
-df = pl.read_csv(tags_csv_path)
+df = pl.read_csv(TAGS_CSV_PATH)
 samples = []
 for species, ratio in SPECIES_RATIO.items():
     df_species = df.filter(pl.col("species") == species)
     num_resource = int(NUM_SAMPLES * ratio)
-    df_species_sample = df_species.sample(num_resource, seed=RAMDOM_SEED)
+    df_species_sample = df_species.sample(num_resource, seed=RAMDOM_SEED, with_replacement=True)
     samples.append(df_species_sample)
 
-df_sample = pl.concat(samples, how="vertical")
+df_sample = pl.concat(samples, how="vertical").unique()
 df_sample.write_csv(output_csv_path, include_bom=True)
