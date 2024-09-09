6f31c204521a75de23580679c44463177ebeaf5e
wsyxbcl <wsyxbcl@gmail.com>
2024-09-09 18:19:55 +0800

    prepare megadetector result for trapper import

--- maze-taxonomy/tag_observations.py
+++ maze-taxonomy/tag_observations.py
@@ -17,6 +17,10 @@ if __name__ == "__main__":
     parser.add_argument(
         "-t", "--tags", type=str, help="Input tags table", required=True
     )
+    # whether the tags table is AI result
+    parser.add_argument(
+        "--ai", action="store_true", help="Whether the tags table is AI result"
+    )
     parser.add_argument(
         "-m", "--media", type=str, help="Input media table", required=True
     )
@@ -32,37 +36,64 @@ if __name__ == "__main__":
     df_observations = df_observations.join(df_media, on="mediaID")
 
     # Patching tags.csv
-    df_tags = pl.read_csv(args.tags)
-    df_tags = df_tags.select(
-        [
-            pl.col("filename")
-            .str.split_exact(".", 1)
-            .struct.rename_fields(["fileName", "extension"]),
-            "species",
-            "individual",
-            pl.col("rating").replace("", "0"),
-        ]
-    ).unnest("filename")
-    df_taglist = pl.read_csv(TAGLIST).select(
-        [pl.col("tag").alias("species"), "scientificName"]
-    )
-    df_tags = df_tags.join(df_taglist, on="species", how="left").drop_nulls()
+    df_tags = pl.read_csv(args.tags, infer_schema_length=0)
+    if not args.ai:
+        df_tags = df_tags.select(
+            [
+                pl.col("filename")
+                .str.split_exact(".", 1)
+                .struct.rename_fields(["fileName", "extension"]),
+                "species",
+                "individual",
+                pl.col("rating").replace("", "0"),
+            ]
+        ).unnest("filename")
+        df_taglist = pl.read_csv(TAGLIST).select(
+            [pl.col("tag").alias("species"), pl.col("mazeScientificName").alias("scientificName")]
+        )
+        print(df_taglist)
+        df_tags = df_tags.join(df_taglist, on="species", how="left")
+        print(df_tags)
 
-    # Tagging observations
-    df_observations_tagged = df_observations.join(df_tags, on="fileName")
-    df_observations_tagged = df_observations_tagged.select(
-        [
-            "_id",
-            "scientificName",
-            pl.lit("animal").alias("observationType"),
-            pl.lit("1").alias("count"),
-            pl.lit("human").alias("classificationMethod"),
-            pl.concat_str(
-                pl.lit("eng: "), pl.col("species"), pl.lit("|Rating:"), pl.col("rating")
-            ).alias("observationTags"),
-            pl.col("individual").alias("individualID"),
-        ]
-    )
+        # Tagging observations
+        # df_observations_tagged = df_observations.join(df_tags, on="fileName")
+        df_observations_tagged = df_tags.join(df_observations, on="fileName")
+        df_observations_tagged = df_observations_tagged.select(
+            [
+                "_id",
+                "scientificName",
+                pl.lit("animal").alias("observationType"),
+                pl.lit("1").alias("count"),
+                pl.lit("human").alias("classificationMethod"),
+                pl.concat_str(
+                    pl.lit("eng: "), pl.col("species"), pl.lit("|Rating:"), pl.col("rating")
+                ).alias("observationTags"),
+                pl.col("individual").alias("individualID"),
+            ]
+        )
+    else:
+        df_tags = df_tags.with_columns(
+            [
+                pl.col("filename")
+                .str.split_exact(".", 1)
+                .struct.rename_fields(["fileName", "extension"]),
+            ]
+        ).unnest("filename")
+        df_observations_tagged = df_tags.join(df_observations, on="fileName")
+        df_observations_tagged = df_observations_tagged.with_columns(
+            [
+                pl.lit("1").alias("count"),
+                pl.lit("machine").alias("classificationMethod"),
+                (
+                    pl.lit("[[") + 
+                    pl.concat_str(
+                        pl.col("bboxX"), pl.col("bboxY"), pl.col("bboxWidth"), pl.col("bboxHeight"), separator=", "
+                    ) +
+                    pl.lit("]]")
+                ).alias("bboxes")
+                # pl.lit("Unidentified").alias("scientificName") # Placeholder for megadetector results
+            ]
+        )
 
     # Save
     df_observations_tagged.write_csv(args.output)
